7bf63e0b6ab2fab002853e8346e720c38cbf2d00
Chen Ozer <chen572@gmail.com>
2020-09-13 20:00:10 +0300

    update

--- src/components/VideoFooter.jsx
+++ src/components/VideoFooter.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import SmsOutlinedIcon from '@material-ui/icons/SmsOutlined';
-import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import React from 'react'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import SmsOutlinedIcon from '@material-ui/icons/SmsOutlined'
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined'
+import { IconButton } from '@material-ui/core'
+import { useUpdateVals } from '../hooks/hooks'
 
 export default function VideoFooter(props) {
-  const { likes, reviews, shares } = props;
+  const { likes, reviews, shares } = props
+  const update = useUpdateVals(props)
 
-  const addLikes = () => {
-    console.log('likes');
-  };
+  const handleClick = async ({ target }) => {
+    console.log(target.id)
+    props[target.id] = await update(target.id, props[target.id] + 1)
+  }
 
-  const addComment = () => {
-    console.log('comment');
-  };
-
-  const addShare = () => {
-    console.log('share');
-  };
   return (
     <div>
-      <FavoriteBorderIcon onClick={addLikes} style={{ marginLeft: '2vh' }} />
-      <span style={{ fontSize: '3vh' }}>{likes}K</span>
-      <SmsOutlinedIcon onClick={addComment} style={{ marginLeft: '4vh' }} />
+      <IconButton onClick={handleClick} style={{ marginLeft: '2vh' }}>
+        <FavoriteBorderIcon id='likes' />
+      </IconButton>
+      <span style={{ fontSize: '3vh' }}>{likes}</span>
+      <IconButton onClick={handleClick} style={{ marginLeft: '2vh' }}>
+        <SmsOutlinedIcon id='reviews' />
+      </IconButton>
       <span style={{ fontSize: '3vh' }}>{reviews}</span>
-      <ShareOutlinedIcon onClick={addShare} style={{ marginLeft: '4vh' }} />
-      <span style={{ fontSize: '3vh' }}>{shares}K</span>
+      <IconButton onClick={handleClick}>
+        <ShareOutlinedIcon id='shares' />
+      </IconButton>
+      <span style={{ fontSize: '3vh' }}>{shares}</span>
     </div>
-  );
+  )
 }
--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -1,8 +1,39 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect, useCallback } from 'react'
+import axios from 'axios'
 
 export function useVideoPlay() {
   const [playing, setPlaying] = useState(false)
   const videoRef = useRef()
+  const videoPlayOnShow = useCallback((node) => {
+    if (videoRef.current) { videoRef.current.disconnect() }
+    videoRef.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        videoRef.current.play()
+      } else {
+        videoRef.current.pause()
+      }
+    })
+    if (node) videoRef.current.observe(node)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     ([entry]) => {
+  //       if (entry.isIntersecting) {
+  //         setPlaying(true)
+  //         videoRef.current.play()
+  //       } else {
+  //         setPlaying(false)
+  //         videoRef.current.pause()
+  //       }
+  //     }
+  //   )
+  //   if (videoRef.current) {
+  //     observer.observe(videoRef.current)
+  //   }
+  // }, [])
 
   const onVideoClick = () => {
     if (playing) {
@@ -14,5 +45,48 @@ export function useVideoPlay() {
     }
   }
 
-  return { videoRef, onVideoClick }
+  return { videoRef, onVideoClick, videoPlayOnShow }
+}
+
+export function useGetProducts() {
+  const [res, setRes] = useState({ data: [], loading: true, error: false })
+
+  useEffect(() => {
+    try {
+      async function getProducts() {
+        return (await axios({
+          url: 'http://localhost:3001/api/v1/products/all',
+          method: 'GET'
+        })).data
+      }
+      getProducts()
+        .then(response => {
+          setRes({ data: response.data, loading: false, error: false })
+        })
+    } catch (e) {
+      setRes({ data: [], loading: false, error: true })
+    }
+  }, [])
+
+  return res
+}
+
+export function useUpdateVals(props) {
+  const { productId, likes, reviews, shares } = props
+  const [values, setValues] = useState({ likes, reviews, shares })
+
+  return async (key, value) => {
+    setValues({
+      ...values,
+      [key]: (await axios({
+        url: 'http://localhost:3001/products/update',
+        method: 'PUT',
+        data: {
+          id: productId,
+          key,
+          value
+        }
+      })).data.data
+    })
+  }
 }
