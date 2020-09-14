9a7d1fdac30365700071869002eabc2b88f5b514
Alon <alonzager5@gmail.com>
2020-09-14 11:00:26 +0300

    paypal button

---
+++ src/components/Paypal.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { PayPalButton } from "react-paypal-button-v2";
+
+export default function Paypal() {
+  return (
+    <div>
+      <PayPalButton
+        amount="0.01"
+        onSuccess={(details, data) => {
+          alert("Transaction completed by " + details.payer.name.given_name);
+          return fetch("/paypal-transaction-complete", {
+            method: "post",
+            body: JSON.stringify({
+              orderID: data.orderID
+            })
+          });
+        }}
+      />
+    </div>
+    
+  );
+}
