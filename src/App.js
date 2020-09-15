74ebe49aeea785ce80096a949d7af7a8188464eb
Alon <alonzager5@gmail.com>
2020-09-15 16:12:07 +0300

    home btn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import ProductsContainer from './components/VideoTest/ProductsContainer'
 import UserPage from './components/UserPage/UserPage'
 import AppFooter from './components/VideoTest/AppFooter'
+import Messages from './components/VideoTest/Messages'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { useUserInfo } from './hooks/hooks'
 
@@ -14,6 +15,7 @@ function App() {
       <Route exact path='/' render={() => <Redirect to='/products'></Redirect>} />
       <Route exact path='/products' render={() => <ProductsContainer />} />
       <Route exact path='/me' render={() => <UserPage userInfo={userInfo} />} />
+      <Route path="/messages" exact render={() => <Messages />} />
       <AppFooter userInfo={userInfo} />
     </Router>
   );
--- src/components/VideoTest/AppFooter.jsx
+++ src/components/VideoTest/AppFooter.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import './AppFooter.css';
 import FacebookLogin from './FacebookLogin';
 import ForumIcon from '@material-ui/icons/Forum';
+import HomeIcon from '@material-ui/icons/Home';
+import { Link } from 'react-router-dom';
 
 export default function AppFooter(props) {
-  const { userInfo } = props
+  const { userInfo } = props;
 
   return (
     <div id='appFooter'>
       <hr></hr>
-      <div id='messagesBtn'>
-            <ForumIcon fontSize="large" />
+      <Link to='messages'>
+        <div className='appFooterBtn'>
+          <ForumIcon fontSize='large' />
         </div>
-      <FacebookLogin />
+      </Link>
+      <Link to='products'>
+      <div className='appFooterBtn'>
+        <HomeIcon fontSize='large' />
+      </div>
+      </Link>
+      <FacebookLogin userInfo={userInfo} />
     </div>
-  )
+  );
 }
