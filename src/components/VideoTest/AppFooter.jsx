2910478ce74bfeb511b628a8af94bf19420c2763
Alon <alonzager5@gmail.com>
2020-09-15 18:18:09 +0300

    fix footer

--- src/components/VideoTest/AppFooter.jsx
+++ src/components/VideoTest/AppFooter.jsx
@@ -8,35 +8,26 @@ import { Link, useLocation } from 'react-router-dom'
 export default function AppFooter(props) {
   const { pathname } = useLocation()
   const { userInfo } = props
-  const [dark, setDark] = useState(false)
 
-  useEffect(() => {
-    if (pathname !== '/products') {
-      setDark(true)
-    } else {
-      setDark(false)
-    }
-  }, [pathname])
 
   return (
     <div id='appFooter'>
-      <hr></hr>
       <div className='icon-container'>
         <Link to='/messages'>
           <div className='appFooterBtn'>
-            <ForumIcon id={dark ? 'dark' : ''} fontSize='large' />
+            <ForumIcon  fontSize='large' />
           </div>
         </Link>
         <Link to='/products'>
           <div
             style={{ marginLeft: 20 }}
-            id={dark ? 'dark' : ''}
+            
             className='appFooterBtn'
           >
             <HomeIcon fontSize='large' />
           </div>
         </Link>
-        <FacebookLogin dark={dark} userInfo={userInfo} />
+        <FacebookLogin  userInfo={userInfo} />
       </div>
     </div>
   )
