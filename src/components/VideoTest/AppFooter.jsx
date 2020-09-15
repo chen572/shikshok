beb8107701fd394915cd0f519a222ba12f35facc
Alon <alonzager5@gmail.com>
2020-09-15 15:35:10 +0300

    messages

--- src/components/VideoTest/AppFooter.jsx
+++ src/components/VideoTest/AppFooter.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './AppFooter.css';
 import FacebookLogin from './FacebookLogin';
-import Messages from './Messages'
+import ForumIcon from '@material-ui/icons/Forum';
 
 
 export default function AppFooter() {
   return (
     <div id='appFooter'>
       <hr></hr>
-      <Messages />
+      <div id='messagesBtn'>
+            <ForumIcon fontSize="large" />
+        </div>
       <FacebookLogin />
     </div>
   );
